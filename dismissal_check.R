4ac2e84106611a738cd0439dd3228673f47b9245
your_username <your_email@example.com>
2015-03-02 17:51:29 -0500

    cleaned draft code

--- dismissal_check.R
+++ dismissal_check.R
@@ -8,14 +8,16 @@ library(stringr)
 df <- laycUtils::load_txt('./temp_data/raw_enrollment_report.txt')
 #ptype <- laycUtils::load_txt('./temp_data/program_type.txt')
 #ptype <- select(ptype, program_id, intervention_type)
-pos <- laycUtils::load_txt('./temp_data/raw_dosage_report.txt')
+pos <- laycUtils::load_txt('./temp_data/raw_pos_report.txt')
 tp <- laycUtils::load_txt('./temp_data/raw_touchpoint_report.txt')
+ref <- laycUtils::load_txt('./temp_data/raw_referrals_report.txt')
+asmt <- laycUtils::load_txt('./temp_data/raw_assessment_report.txt')
 
 # clean data
 # df
-headers <- names(df)
-headers[1] <- 'id'
-names(df) <- headers
+# headers <- names(df)
+# headers[1] <- 'id'
+# names(df) <- headers
 df$days_enrolled <- as.numeric(df$days_enrolled)
 
 keep <- !str_detect(df$program_name, '^rb -')
@@ -35,7 +37,21 @@ tp %>%
   tp
 tp$service <- 1
 
-service <- bind_rows(pos, tp)
+# asmt
+asmt %>%
+  select(id = subject_id, program_id) %>%
+  distinct ->
+  asmt
+asmt$service <- 1
+
+# ref
+ref %>%
+  select(id = subject_id, program_id) %>%
+  distinct ->
+  ref
+ref$service <- 1
+
+service <- bind_rows(pos, tp, asmt, ref)
 service <- unique(service)
 
 # merge
@@ -45,11 +61,34 @@ df %>%
   left_join(service, by = c('id', 'program_id')) ->
   test
 
-test %>% 
+test %>%
   filter(is.na(service)) ->
   issue
 
-test %>% 
+# Save list of participants with issue
+programs <- sort(unique(issue$program_name))
+
+for (program in programs) {
+  out <- issue[issue$program_name == program, ]
+  write.csv(out, file = paste0('./output/', program, '.csv'), row.names = FALSE)
+}
+
+
+issue %>%
+  group_by(program_name) %>%
+  summarise(n = length(id)) %>%
+  arrange(desc(n)) %>%
+  filter(n > 20) ->
+  issue_summary
+
+my_title <- 'Number of participants active in a program during FY14\nwithout any other type of information recorded in ETO\n'
+p <- ggplot(issue_summary, aes(x = reorder(program_name, n), y = n)) + coord_flip()
+p <- p + geom_point()
+p <- p + ggtitle(my_title)
+p <- p + ylim(c(0, 200))
+p
+
+test %>%
   filter(!is.na(service)) %>%
   distinct(id) %>%
   count()
--- draft.R
+++ draft.R
@@ -4,26 +4,52 @@ list.files('./temp_data/')
 library(dplyr)
 library(stringr)
 
+# Set constants
+end_date <- lubridate::mdy('9/30/2014')
+start_date <- lubridate::mdy('1/1/2008')
+
 # Load data
 df <- laycUtils::load_txt('./temp_data/raw_enrollment_report.txt')
 ptype <- laycUtils::load_txt('./temp_data/program_type.txt')
 ptype <- select(ptype, program_id, intervention_type)
-dosage <- laycUtils::load_txt('./temp_data/raw_dosage_report.txt')
+dosage <- laycUtils::load_txt('./temp_data/raw_pos_report.txt')
 
 # Merge data
 df <- dplyr::left_join(df, ptype, by = c('program_id'))
 
 # clean data
-headers <- names(df)
-headers[1] <- 'id'
-names(df) <- headers
+#headers <- names(df)
+#headers[1] <- 'id'
+#names(df) <- headers
 df$days_enrolled <- as.numeric(df$days_enrolled)
 
 keep <- !str_detect(df$program_name, '^rb -')
 df <- filter(df, keep)
 
+#df %>% filter(id == '64,392') -> df
+
+# Clean end date
+df$end <- lubridate::mdy(df$end)
+df$end[is.na(df$end)] <- end_date
+# Clean start date
+df$start <- lubridate::mdy(df$start)
+df$start[df$start < start_date] <- start_date
+
+span <- lubridate::new_interval(df$start, df$end) #interval
+
+df$days <- lubridate::as.period(span, units = "day")
+df$days <- lubridate::day(df$days)
+
+ggplot(df, aes(x = days)) + geom_histogram() + facet_wrap(~intervention_type)
+
+
+
+
+
+
 # Number of participants served by program area
 df %>%
+  filter(program_name != 'ss - case management') %>%
   group_by(intervention_type) %>%
   summarise(n = length(id)) ->
   test
