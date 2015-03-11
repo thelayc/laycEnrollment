de9f794a06f7b1b4be8fbadf82e28e81766ebc91
your_username <your_email@example.com>
2015-03-11 09:22:23 -0400

    Removed helper functions (put them in their own file)

--- R/get_enrollment.R
+++ R/get_enrollment.R
@@ -16,7 +16,7 @@ get_enrollment <- function(enroll_data, program_type,
                            start_date = '01/01/2008',
                            end_date = Sys.Date()) {
   # Clean data
-  enroll_data <- clean_df(enroll_data, program_type, start_date = '01/01/2008', end_date = Sys.Date())
+  enroll_data <- clean_df(enroll_data, program_type, start_date = start_date, end_date = end_date)
   # Generate summary table
   enroll_summary <- get_summary(enroll_data)
 
@@ -24,59 +24,7 @@ get_enrollment <- function(enroll_data, program_type,
 
 
 
-#' clean_df()
-#'
-#' Helper function to tackle data cleaning for the get_enrollment function
-#' @param enroll_data dataframe: a dataframe containing enrollment data.[INSERT REPORT NAME HERE]
-#' @param program_type dataframe: a dataframe containing information about each program intervention type. [INSERT REPORT NAME HERE]
-#' @param start_date character: All program start dates in enroll_data must be posterior or equal to this date. Date must be specified as "mm/dd/yyyy"
-#' @param end_date character: All program end dates in enroll_data must be anterior or equal to this date. Date must be specified as "mm/dd/yyyy"
-#' @return dataframe
-
-clean_df <- function(enroll_data, program_type,
-                           start_date = '01/01/2008',
-                           end_date = Sys.Date()) {
-  # Remove extra columns for a cleaner merge with enroll_data
-  program_type <- dplyr::select_(program_type, ~program_id, ~intervention_type)
-  # Change days_enrolled to numeric for plottingand computations
-  enroll_data$days_enrolled <- as.numeric(enroll_data$days_enrolled)
-  # Clean end date
-  if(class(end_date) == "Date") {
-    end_date <- as.character(end_date)
-    end_date <- lubridate::ymd(end_date)
-  } else {
-      end_date <- lubridate::mdy(end_date)
-    }
-  enroll_data$end <- lubridate::mdy(enroll_data$end)
-  enroll_data$end[is.na(enroll_data$end)] <- end_date
-  enroll_data$end[enroll_data$end > end_date] <- end_date
-  # Clean start date
-  start_date <- lubridate::mdy(start_date)
-  enroll_data$start <- lubridate::mdy(enroll_data$start)
-  enroll_data$start[enroll_data$start < start_date] <- start_date
-  # Compute enrollment time (in days)
-  span <- lubridate::new_interval(enroll_data$start, enroll_data$end) #interval
-  enroll_data$days <- lubridate::as.period(span, units = "day")
-  enroll_data$days <- lubridate::day(enroll_data$days)
-  # Merge dataframes
-  enroll_data <- dplyr::left_join(enroll_data, program_type, by = c('program_id'))
-
-  return(enroll_data)
-}
 
 
-#' get_summary()
-#'
-#' Helper function to compute summary statistics about enrollment, by program type (education, workforce, etc.)
-#' @param df dataframe: a dataframe returned by the clean_df() function
-#' @return dataframe
 
-get_summary <- function(df){
-    df %>%
-    group_by_(~intervention_type) %>%
-    summarise_(n = ~length(id),
-              av_days = ~mean(days),
-              std_days = ~sd(days)) ->
-    df
-}
 
