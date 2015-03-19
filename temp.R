716438a82ff45ec38ab339e12bf158d76dfaa93b
your_username <your_email@example.com>
2015-03-19 17:08:51 -0400

    updated temporary code

--- temp.R
+++ temp.R
@@ -3,6 +3,8 @@ library(laycUtils)
 library(laycEnrollment)
 library(dplyr)
 library(stringr)
+library(extrafont)
+library(ggplot2)
 # Load and basic cleaning
 enroll_data <- laycUtils::load_txt('../temp_data/fy14/raw_enrollment_report.txt')
 enroll_data <- laycUtils::clean_data(enroll_data)
@@ -16,5 +18,11 @@ enroll_data <- clean_enroll(enroll_data, program_type,
                             start_date = '01/01/2008',
                             end_date = '09/30/2014')
 # Pull summary statistics
-get_summary(enroll_data)
-
+summary <- get_summary(enroll_data)
+p <- plot_summary(summary)
+ggsave(filename = '../temp_data/fy14/output/enrollment_chart_fy4.png',
+       plot = p,
+       units = 'cm',
+       width = 30,
+       height = 15)
+histo <- get_histogram(enroll_data)
